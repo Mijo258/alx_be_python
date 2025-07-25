8462735960f576bf26e224e566b4675ed58bee0c
mohammed <mijogiddo@gmail.com>
2025-07-25 20:46:32 +0300

    1 of 5

--- basic_operations.py
+++
@@ -1,6 +0,0 @@
-number1 = 10 
-number2 = 5 
-print(f"addition of {number1} and {number2} is {number1 + number2}")
-print(f"subtraction of {number1} and {number2} is {number1 - number2}")
-print(f"multiplication of {number1} and {number2} is {number1 * number2}")
-#print(f"division of {number1} and {number2} is {number1 / number2}")
