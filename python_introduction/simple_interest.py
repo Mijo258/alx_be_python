f15962e9ca604a1645986efd21ebb5b69994be54
mohammed <mijogiddo@gmail.com>
2025-07-25 09:40:55 +0300

    dum dum checker again

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,5 @@
+principal = 1000  # Principal amount in dollars
+rate = 0.05  # Annual interest rate in percent
+time = 3  # Time in years
+interest = principal * rate * time  # Simple interest formula
+print(f"The simple interest is: {interest:.1f}")
