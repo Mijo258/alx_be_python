22130ea689886f2c15896eeb84029f1aa5dcf20f
mohammed <mijogiddo@gmail.com>
2025-08-10 16:17:23 +0300

    great implementation

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,15 +1,33 @@
+#!/usr/bin/python3
+"""
+This module defines the BankAccount class for simple banking operations.
+"""
+
+
 class BankAccount:
-    def __init__(self, account_balance = 0):
+    """A class to represent a simple bank account."""
+
+    def __init__(self, account_balance=0):
+        """Initializes a new BankAccount instance."""
         self.account_balance = int(account_balance)
 
     def deposit(self, amount):
+        """Adds a specified amount to the account balance."""
         self.account_balance += int(amount)
+        # It's good practice to return the new balance
+        return self.account_balance
 
     def withdraw(self, amount):
-        if amount > self.account_balance:
+        """
+        Withdraws a specified amount if funds are sufficient.
+        Returns True for success, False for failure.
+        """
+        if int(amount) > self.account_balance:
             return False
         else:
             self.account_balance -= int(amount)
             return True
+
     def display_balance(self):
-            return self.account_balance
+        """Returns the current account balance."""
+        return self.account_balance
