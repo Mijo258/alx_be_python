3268d5eb11d7fac01311e6fda368114221c4fcd6
mohammed <mijogiddo@gmail.com>
2025-08-10 15:48:30 +0300

    very important implementation git add .

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,15 @@
+class BankAccount :
+    def __init__(self, account_balance = 0):
+        self.account_balance = int(account_balance)
+
+    def deposit(self, amount):
+        self.account_balance += int(amount)
+
+    def withdraw(self, amount):
+        if amount > self.account_balance:
+            return False
+        else:
+            self.account_balance -= int(amount)
+            return True
+    def display_balance(self):
+            return self.account_balance
