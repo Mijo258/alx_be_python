a064a1c651c3d6af644eeb12cb61ed6444b1d666
mohammed <mijogiddo@gmail.com>
2025-08-21 03:36:59 +0300

    easy polymorphism task

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,24 @@
+import math
+class Shape:
+    """Base class for all shapes."""
+    def area(self):
+        """Returns the area of the shape."""
+        raise NotImplementedError("Subclasses must implement this method.")
+    
+class Rectangle(Shape):
+    """Class representing a rectangle."""
+    def __init__(self, width: float, height: float):
+        self.width = width
+        self.height = height
+    def area(self):
+        """Calculates the area of the rectangle."""
+        return self.width * self.height
+    
+class Circle(Shape):
+    """Class representing a circle."""  
+    def __init__(self, radius: float):
+        self.radius = radius    
+    def area(self):
+        """Calculates the area of the circle."""
+        return math.pi * (self.radius ** 2)
+    
